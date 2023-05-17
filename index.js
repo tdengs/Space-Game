672fe477e9c6d1ad06672335356c15ac0c9c2111
tdengs <tehdengshyang@gmail.com>
2023-05-18 00:16:13 +0100

    Completed game object classes

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+// set up class objects
+// set up game object class
+class GameObject {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.type = '';
+        this.dead = false;
+        this.width = 0;
+        this.height = 0;
+        this.img = '';
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+}
+
+// set up player object class
+class Player extends GameObject {
+    constructor(x, y) {
+        super(x, y);
+        this.type = 'Player';
+        (this.width = 99), (this.height = 75);
+        this.speed = { x: 0, y: 0 };
+    }
+}
+
+// set up enemy object class
+class Enemy extends GameObject {
+    constructor(x, y) {
+        super(x, y);
+        this.type = 'Enemy';
+        (this.width = 98), (this.height = 50);
+        let id = setInterval(() => {
+            if (this.y < canvas.height - this.height) {
+                this.y += 5;
+            } else {
+                console.log('Stopped at', this.y);
+                clearInterval(id);
+            }
+        }, 300)
+    }
+}
+
 // set up an event emitter class that contains listeners using pub-sub pattern
 class EventEmitter {
     constructor() {
@@ -20,77 +65,43 @@ class EventEmitter {
     }
 }
 
-// set up game object class
-class GameObject {
-    constructor(x, y, type) {
-        this.x = x;
-        this.y = y;
-        this.type = type;
-    }
-}
 
-// set up movable object class
-class MovableObject extends GameObject {
-    constructor(x, y, type) {
-        super(x, y, type)
-    }
-
-    moveTo(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-}
-
-// set up player object class
-class Player extends MovableObject {
-    constructor(x, y) {
-        super(x, y, "Player");
-    }
-}
-
-// set up enemy object class
-class Enemy extends MovableObject {
-    constructor(x, y) {
-        super(x, y, "Enemy");
-    }
-}
 
 // set up a message structure
 const Messages = {
-    HERO_MOVE_LEFT: 'HERO_MOVE_LEFT',
-    HERO_MOVE_RIGHT: 'HERO_MOVE_RIGHT'
+    PLAYER_MOVE_LEFT: ' PLAYER_MOVE_LEFT',
+    PLAYER_MOVE_RIGHT: 'PLAYER_MOVE_RIGHT',
+    PLAYER_MOVE_UP: 'PLAYER_MOVE_UP',
+    PLAYER_MOVE_DOWN: 'PLAYER_MOVE_DOWN',
 };
 
-// instantiate event emitter
 const eventEmitter = new EventEmitter();
-
-// instantiate player
-const player = new Player(0, 0);
-
-// let the event emitter know to watch for messages pertaining to movements of player, and act on it
-eventEmitter.subscribe(Messages.HERO_MOVE_LEFT, () => player.moveTo(5, 0));
-eventEmitter.subscribe(Messages.HERO_MOVE_RIGHT, () => player.moveTo(-5, 0));
 
 // set up the window to listen for the key up event
 window.addEventListener('keyup', (evt) => {
     if (evt.key === 'ArrowLeft') {
-        eventEmitter.publish(Messages.HERO_MOVE_LEFT);
+        eventEmitter.publish(Messages.PLAYER_MOVE_LEFT);
     }
     else if (evt.key === 'ArrowRight') {
-        eventEmitter.publish(Messages.HERO_MOVE_RIGHT);
+        eventEmitter.publish(Messages.PLAYER_MOVE_RIGHT);
+    }
+    else if (evt.key === 'ArrowUp') {
+        eventEmitter.publish(Messages.PLAYER_MOVE_UP);
+    }
+    else if (evt.key === 'ArrowDown') {
+        eventEmitter.publish(Messages.PLAYER_MOVE_DOWN);
     }
 });
 
 // create 5x5 enemies
-function createEnemies(ctx, canvas, enemyImg){
+function createEnemies(ctx, canvas, enemyImg) {
     const MONSTER_TOTAL = 5;
     const MONSTER_WIDTH = MONSTER_TOTAL * 98;
-    const START_X = (canvas.width - MONSTER_WIDTH)/2;
+    const START_X = (canvas.width - MONSTER_WIDTH) / 2;
     const STOP_X = START_X + MONSTER_WIDTH;
 
-    for (let x = START_X; x < STOP_X; x += 98){
-        for (let y = 0; y < 50 * 5; y += 50){
+    for (let x = START_X; x < STOP_X; x += 98) {
+        for (let y = 0; y < 50 * 5; y += 50) {
             ctx.drawImage(enemyImg, x, y);
         }
     }
@@ -99,16 +110,16 @@ function createEnemies(ctx, canvas, enemyImg){
 // load assets
 function loadAsset(path) {
     return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = path;
-      img.onload = () => {
-        // image loaded and ready to be used
-        resolve(img);
-      }
-      img.onerror = () => {
-        // image failed to load
-        reject(new Error(`Image failed to load: ${path}`));
-      }
+        const img = new Image();
+        img.src = path;
+        img.onload = () => {
+            // image loaded and ready to be used
+            resolve(img);
+        }
+        img.onerror = () => {
+            // image failed to load
+            reject(new Error(`Image failed to load: ${path}`));
+        }
     })
 }
 
@@ -124,18 +135,18 @@ window.onload = async () => {
     } catch (error) {
         console.log('Error:', error);
     }
-    
+
     // colour background black
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // draw player and hero images if loaded successfully
-    if(playerImg && enemyImg){
-        ctx.drawImage(playerImg, canvas.width/2 - 45, canvas.height - canvas.height/4);
+    if (playerImg && enemyImg) {
+        ctx.drawImage(playerImg, canvas.width / 2 - 45, canvas.height - canvas.height / 4);
         createEnemies(ctx, canvas, enemyImg);
     }
 }
-  
+
 
 
 
